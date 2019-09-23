c722784162e338dbbe3b58aa13fdd1653f37748f
Lux <luxi@didichuxing.com>
2019-09-23 16:03:28 +0800

    [描述]：添加 UIView 测试基类

---
+++ FrameworksDemoProject/FrameworksDemoProject/Frameworks/UIKit/UIView/FDUIViewViewController.h
@@ -0,0 +1,17 @@
+//
+//  FDUIViewViewController.h
+//  FrameworksDemoProject
+//
+//  Created by Lux on 2019/9/23.
+//  Copyright © 2019 Lux. All rights reserved.
+//
+
+#import "FDBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FDUIViewViewController : FDBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FrameworksDemoProject/FrameworksDemoProject/Frameworks/UIKit/UIView/FDUIViewViewController.m
@@ -0,0 +1,22 @@
+//
+//  FDUIViewViewController.m
+//  FrameworksDemoProject
+//
+//  Created by Lux on 2019/9/23.
+//  Copyright © 2019 Lux. All rights reserved.
+//
+
+#import "FDUIViewViewController.h"
+
+@interface FDUIViewViewController ()
+
+@end
+
+@implementation FDUIViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
